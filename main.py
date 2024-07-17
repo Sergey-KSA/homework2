0606e230421e0295e1a993d5a2ad582a1b05f6e6
Sergey-KSA <77i7@ro.ru>
2024-07-18 00:08:32 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,5 @@
+kolichestvo_vipolnenih_DZ = 12
+kolichestvo_zatrachenih_chasov = 1.5
+nazvanie_kursa = 'Python'
+vremya_na_odno_zadanie = kolichestvo_vipolnenih_DZ/kolichestvo_zatrachenih_chasov
+print ('Курс:', nazvanie_kursa, ',', 'Всего задач:',kolichestvo_vipolnenih_DZ, ',', 'Затрачено часов: ', kolichestvo_zatrachenih_chasov, ',', 'среднее время выполнения: ', kolichestvo_zatrachenih_chasov/kolichestvo_vipolnenih_DZ)
