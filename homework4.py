1ce4634c30f43d1aec01db87377150e0da38ddf5
Sergey-KSA <77i7@ro.ru>
2024-07-28 11:28:04 +0400

    Add files via upload

---
+++ homework4.py
@@ -0,0 +1,9 @@
+my_string = input ('Vvedite text: ')
+print(type(my_string))
+print (len(my_string))
+print (my_string.upper())
+print(my_string.lower())
+my_string='Vasya prishel v kafe Veterok'
+print (my_string .replace (" ",""))
+print (my_string [0])
+print (my_string [-1])
