d13c0ac6ffab166edf459c6c43193a4481fa016d
Sergey-KSA <77i7@ro.ru>
2024-08-02 22:47:20 +0300

    Add files via upload

---
+++ module_1_6.py
@@ -0,0 +1,12 @@
+my_dict ={'Med':4, 'Chugun':57,'Stal':23, 'Uran':97}
+print (my_dict )
+print(my_dict .get ('Stal', 'frog'))
+my_dict .update ({'Voda':9, 'moloko':3, 'gaz':0.1})
+del my_dict ['Chugun']
+print (my_dict.get('Chugun') )
+print(my_dict)
+print(my_dict.pop ('Uran'))
+my_set ={11,23,23,'torn',True,1,11,True, (6,8,0)}
+print (my_set)
+my_set.update ('Dub')
+print(my_set)
