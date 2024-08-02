7301c7046620cc4760a6c740dc38c580cda7ede9
Sergey-KSA <77i7@ro.ru>
2024-08-02 22:09:59 +0300

    Add files via upload

---
+++ module_1_5.py
@@ -0,0 +1,11 @@
+immutable_var = 1,3,4,'Drevo', 2.7, False
+print(immutable_var)
+#immutable_var [3]=6 #Кортеж неименяем
+mutable_list = [1, 5.4, True, 'Ozero']
+print (mutable_list)
+mutable_list [1] = 4
+mutable_list [2] = False
+print(mutable_list )
+mutable_list =(6,8,['Ozero', 'O'])
+mutable_list [2][0] = '100'
+print(mutable_list )
